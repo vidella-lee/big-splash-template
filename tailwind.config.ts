883b04c9d2166c59505f8344980fce705ae4f30e
Elizabeth Nelson <lee.anewed@gmail.com>
2025-06-04 07:01:33 -0500

    styling; layout; media

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+const config: Config = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,7 +9,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        dark: "#1A1313",
+        light: "#DCD3D2",
+        babyBlue: "#43AEFB", // primary
+        muddyGreen: "#143236", // secondary
+        slateGreen: "#3B545E",
+        rouge: "#D92525", //danger
+        // primary: ,
+        // secondary: ,
+        // info: ,
+        // success: ,
+        // danger: #6F5151,
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        leagueGothic: ["var(--font-league-gothic)"],
+      },
       backgroundImage: {
+        "hero-image": "url('/network-vid-white.mp4')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
@@ -16,5 +35,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+});
 export default config;
